269c00e7fab2d0ad1a879fe9e60f468f3a113e9e
DhruvRathod1 <dhruvrathod195@gmail.com>
2024-07-07 20:43:28 +0530

    update technology

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,15 +6,18 @@ import { companies } from "@/data";
 const Clients = () => {
   return (
     <section id="testimonials" className="py-20">
-      <h1 className="heading">
+      <h1 className="heading text-center mb-10">
         My
         <span className="text-purple"> Tech Stack</span>
       </h1>
 
-      <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+      <div className="h-auto md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map((company) => (
-            <div key={company.id} className="flex justify-center items-center w-12 h-12 md:w-32 md:h-32">
+            <div 
+              key={company.id} 
+              className="flex justify-center items-center w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28"
+            >
               <img
                 src={company.img}
                 alt={company.name}
