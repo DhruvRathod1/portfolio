b61a7bc2403f35b1a71ed808bfb83ed2bb37ca49
DhruvRathod1 <dhruvrathod195@gmail.com>
2024-07-09 23:29:58 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -90,7 +90,7 @@ export const projects = [
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
+    title: "Quiz Applicaton - ReactJs",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
